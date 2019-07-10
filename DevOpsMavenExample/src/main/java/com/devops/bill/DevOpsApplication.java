3a58f5f9a69bf863f8eeb94ac4e4887c555ff7b0
wasantamaria <william_santa_maria@yahoo.com>
2019-07-10 06:39:14 -0400

    initial
    Adding example maven project

---
+++ DevOpsMavenExample/src/main/java/com/devops/bill/DevOpsApplication.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.devops.bill;
+
+/**
+ * @author w.a.santa.maria
+ *
+ */
+public class DevOpsApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello DevOps Maven Example.");
+	}
+
+}
